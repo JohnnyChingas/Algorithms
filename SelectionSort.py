a4d9aaa52eca5aca45f237506e711f0897f21f4c
Tim Melano <tmelano@gmail.com>
2014-02-15 14:41:50 -0800

    Cleaned up SelectionSort

--- SelectionSort.py
+++ SelectionSort.py
@@ -11,18 +11,9 @@ def selection_sort(list):
         print list
         min = i;
         for j in xrange(i+1,n):
-            print "j = %d, min = %d"%(j,min)
-            print "is %s < %s?"%(list[j],list[min])
             if list[j] < list[min]:
-                print "%s is smaller than %s"%(list[j],list[min])
                 min = j
-            else:
-                print "No"
-
         list[min], list[i] = list[i], list[min]
-        print "let's swap ind %d for ind %d"%(i,min)
-        print list
-        print "\n"
 
 if __name__ == "__main__":
     main()
