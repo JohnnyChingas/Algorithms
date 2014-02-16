9092f2af1d91b691370880f3451f0dd19ba5a19c
Tim Melano <tmelano@gmail.com>
2014-02-16 15:34:06 -0800

    Delete node implemented

--- BinaryTree.py
+++ BinaryTree.py
@@ -30,48 +30,82 @@ class Node:
             else:
                 self.right.insert(data)
         
-    def search(self, data):
+    def search(self, data, parent = None):
         """
         Search tree for data
         @param data object
+        returns object containing data
         """
         if data < self.data:
             if self.left is None:
-                return None
-            return self.left.search(data)
+                return None, None
+            return self.left.search(data, self)
         elif data > self.data:
             if self.right is None:
                 return None
-            return self.right.search(data)
+            return self.right.search(data, self)
         else:
-            return self.data
+            return self, parent
 
     def min(self):
         if self.left is not None:
             return self.left.min() 
         else:
-            return self.data
+            return self
                 
     def max(self):
         if self.right is not None:
             return self.right.max()
         else:
-            return self.data
+            return self
+
+    def traverse(self):
+        if self.left is not None:
+            self.left.traverse()
+        print self.data
+        if self.right is not None:
+            self.right.traverse()
+
+    def parent(self,data):
+        # write this
+        return true
+    
+    def delete(self, data):
+        headNode,parent = self.search(data)
+        if headNode.left and headNode.right:
+            succesor = headNode.right.min()
+            succesor,parent = self.search(succesor.data)
+            headNode.data = succesor.data
+            if parent.left != None:
+                if parent.left.data == succesor.data:
+                    parent.left = None
+            elif parent.right != None:
+                if parent.right.data == succesor.data:
+                    parent.right = None
+            del succesor
+        else:
+            headNode.data = headNode.left.data or headNode.right.data
+            headNode.left, headNode.right = None, None
 
 def main():
     root = Node(8)
     data_list = [3,10,1,6,4,7,14,13]
     for d in data_list:
         root.insert(d)
-    """
     print root.search(14)
     print root.search(1)
     print root.search(7)
     print root.search(9)
     print root.search(None)
-    """
     print root.min()
     print root.max()
+
+    root.traverse()
+    delete_node = 14
+    print "deleting node ",delete_node
+    root.delete(delete_node)
+    root.traverse()
+
 
 if __name__ == '__main__':
     main()
